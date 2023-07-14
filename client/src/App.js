fa7adddf21c5e49e736154b182b0b34f57c9376d
Nakul Prasad <nakulprasad10@outlook.com>
2023-07-14 14:45:03 +0530

    darkmode theme setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { themeSettings } from "theme";
+
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  //themesetting return an object with pallete and typography with modeSelected
+  //useMemo : rerenders App() when mode is changed
+  //saving generated theme in cache
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-
+      <ThemeProvider theme={theme}>
+        {/* CssBaseLIne remove default mui css */}
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   )
 }
