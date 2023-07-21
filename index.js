ee136a3bdc822b120cf8af27cae84ec984fca71f
Nakul Prasad <nakulprasad10@outlook.com>
2023-07-21 19:45:18 +0530

    added base route

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* Base Route */
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
 /* ROUTES */
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
